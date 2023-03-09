bb98a1315a59d7391467a4b73680a7c902c66f7e
Julia_Ivanova <109332548+Julia27Ivanova@users.noreply.github.com>
2023-03-09 23:04:17 +0200

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,66 @@
+'use stict';
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+//1.Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+//2.Реалізація делегування на div.gallery і отримання url великого зображення.
+//3.Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. 
+//Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
+//4.Відкриття модального вікна по кліку на елементі галереї. 
+//Для цього ознайомся з документацією і прикладами.
+//5.Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. 
+//Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
 
 console.log(galleryItems);
+const galleryImg = document.querySelector(`.gallery`);
+const imagesMarkup = createGalleryMarkup(galleryItems);
+galleryImg.insertAdjacentHTML('beforeend', imagesMarkup);
+galleryImg.addEventListener('click', onImageClick);
+let lightbox;
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+    })
+    .join('');
+}
+
+function stopLoadingImg(e) {
+  e.preventDefault();
+}
+
+function onEscapePress(e) {
+  if (e.code === 'Escape') {
+    this.close();
+  }
+}
+
+function onImageClick(e) {
+  stopLoadingImg(e);
+
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  lightbox = basicLightbox.create(`<img src="${e.target.dataset.source}">`, {
+    onShow: lightbox => {
+      document.addEventListener('keydown', onEscapePress.bind(lightbox), {
+        once: true,
+      });
+    },
+    onClose: () => {
+      document.removeEventListener('keydown', onEscapePress);
+    },
+  });
+
+  lightbox.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
+'use stict';
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryImg = document.querySelector('.gallery');
+const imagesMarkup = createGalleryMarkup(galleryItems);
+galleryImg.insertAdjacentHTML('beforeend', imagesMarkup);
+galleryImg.addEventListener('click', onImageClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>
+  </div>`;
+    })
+    .join('');
+}
+
+function stopLoadingImg(e) {
+  e.preventDefault();
+}
+
+function summonSimpleLightbox() {
+  new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
+
+function onImageClick(e) {
+  stopLoadingImg(e);
+
+  if (e.target.nodeName === 'IMG') {
+    summonSimpleLightbox();
+  }
+}
 
 console.log(galleryItems);
